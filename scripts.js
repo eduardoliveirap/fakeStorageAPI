14e6536448b2398c2fb507ac8448942aa0be5fa5
eduardoliveirap <eduardoliveirap@hotmail.com>
2023-04-05 09:48:32 -0300

    filter function half completed

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
-function buscarCategorias()
+    function buscarCategorias()
 {
     fetch("https://fakestoreapi.com/products/categories")
     .then(res => res.json())
     .then(listaCategorias => {
+    
         listaCategorias.forEach(cada => {
             categorias.innerHTML += `
             <li class="list-group-item">
             <h6>
-            <input type="checkbox">
+            <input name="${cada}" type="checkbox" onClick="filtro()">
             <span>${cada}</span>
         </h6>
         </li>`
@@ -19,12 +20,15 @@ function buscarProdutos() {
     fetch('https://fakestoreapi.com/products')
     .then(res => res.json()) 
     .then(listaProdutos => {
+        lista = listaProdutos;
         listaProdutos.forEach(cada => {
-            products.innerHTML += `<li>
+            products.innerHTML += `
+            <li class="${cada.category.replace(" ","-")}">
             <div class="card">
                 <img src="${cada.image}" alt="" class="card-img-top">
                 <div class="card-body">
-                    <h5 class="card-title">${cada.title}</h5>
+                    <h5 class="card-title" title="${cada.title}">${cada.title}</h5>
+                    <div class="badge bg-secondary mb-2">${cada.category}</div>
                     <p class="card-text text-wrap">${cada.description}</p>
                     <a href="" class="btn btn-primary">Adicionar ao carrinho</a>
                 </div>
@@ -33,3 +37,18 @@ function buscarProdutos() {
         });
     });
 }buscarProdutos();
+
+function filtro(){
+    let inputs = document.querySelectorAll('input:checked')
+    let input_nomes = [];
+    for(let i = 0; i < inputs.length; i++){
+        input_nomes.push(inputs[i].name);
+    }
+
+    let lis = document.querySelectorAll("#products li");
+    for(let i= 0; i< lis.length; i++) {
+        if(!lis[i].classList.contains(input_nomes.toString())){
+            lis[i].style.display = 'none'
+        }
+    }
+}
